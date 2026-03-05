d3ae6c7fb263521124c8928ea9b8734df8c36b0a
Carlos Nathaniel <Carlosnathaniel20@gmail.com>
2026-03-05 22:48:37 +0800

    Fixed API key

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
 
 from .views import MyTokenObtainPairView, register_view
 
 urlpatterns = [
     path('signup/', register_view, name='signup'),
     path('signin/', MyTokenObtainPairView.as_view(), name='signin'),
+    path('signin/refresh/', TokenRefreshView.as_view(), name='signin_refresh'),
 ]
